e48887f8869c164eed78732ec6497df282162f1d
Gabriel <147839286+MeijinEx1a@users.noreply.github.com>
2025-01-03 01:36:04 +0000

    Created GcashApp UserAuth

--- GCashApp/UserAuthentication.java
+++ GCashApp/UserAuthentication.java
@@ -1,5 +1,21 @@
 package GCashApp;
 
+
+class User {
+    private int id;
+    private String name, email, number, pin;
+
+    public User(int id, String name, String email, String number, String pin) {
+        this.id = id; this.name = name; this.email = email; this.number = number; this.pin = pin;
+    }
+
+    public int getId() { return id; }
+    public String getEmail() { return email; }
+    public String getPin() { return pin; }
+    public void setPin(String pin) { this.pin = pin; }
+    public String getName() { return name; }
+}
+
 public class UserAuthentication {
     
 }
--- Testers/SimpleCalculatorTest.java
+++ Testers/SimpleCalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 
 public class SimpleCalculatorTest {
     @Test
